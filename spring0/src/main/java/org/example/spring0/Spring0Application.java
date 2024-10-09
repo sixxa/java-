007faf25d16531736cf556a34da6d1c2e8c46aa6
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-10-09 03:26:25 +0200

    lets start experimenting

---
+++ spring0/src/main/java/org/example/spring0/Spring0Application.java
@@ -0,0 +1,15 @@
+package org.example.spring0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Spring0Application.class, args);
+
+    }
+
+}
