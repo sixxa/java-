23a2bfcdbfc580bc6174d0a5783f2e2ab565ed10
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-10-14 18:56:32 +0200

    done

--- springJDBC/src/main/java/org/example/springjdbc/SpringJdbcApplication.java
+++ springJDBC/src/main/java/org/example/springjdbc/SpringJdbcApplication.java
@@ -1,13 +1,28 @@
 package org.example.springjdbc;
 
+import org.example.springjdbc.model.Alien;
+import org.example.springjdbc.rep.AlienRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJdbcApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+
+        Alien alien1 = context.getBean(Alien.class);
+        alien1.setId(111);
+        alien1.setName("Toka");
+        alien1.setTech("java");
+
+        AlienRepo repo = context.getBean(AlienRepo.class);
+        repo.save(alien1);
+
+        System.out.println(repo.findAll());
     }
 
 }
---
+++ springJDBC/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'Navin', 'Blockchain');
+insert into alien (id, name, tech) values (102, 'Nika', 'AI');
+insert into alien (id, name, tech) values (103, 'Ana', 'IoT');
