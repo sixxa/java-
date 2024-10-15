8a22d75d6a5ebfe4d296fa826c07b91cd4584271
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-10-15 13:33:54 +0200

    simplest spring boot jsp project

---
+++ springMVCBoot0/src/main/java/org/example/springmvcboot0/SpringMvcBoot0Application.java
@@ -0,0 +1,13 @@
+package org.example.springmvcboot0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBoot0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBoot0Application.class, args);
+    }
+
+}
---
+++ springMVCBoot0/src/test/java/org/example/springmvcboot0/SpringMvcBoot0ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmvcboot0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBoot0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
