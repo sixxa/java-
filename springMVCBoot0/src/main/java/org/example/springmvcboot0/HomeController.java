b30abccad9a522749db8ff69aabc9dfb4d285390
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-10-17 02:25:35 +0200

    simplified versin using modelandview

--- springMVCBoot0/src/main/java/org/example/springmvcboot0/HomeController.java
+++ springMVCBoot0/src/main/java/org/example/springmvcboot0/HomeController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -13,13 +15,10 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest req){
-        int i = Integer.parseInt(req.getParameter("num1"));
-        int j = Integer.parseInt(req.getParameter("num2"));
-
+    public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j){
+        ModelAndView mv = new ModelAndView("result");
         int sum = i+j;
-        HttpSession session = req.getSession();
-        session.setAttribute("sum", sum);
-        return "result";
+        mv.addObject("sum",sum);
+        return mv;
     }
 }
--- springMVCBoot0/src/main/webapp/WEB-INF/result.jsp
+++ springMVCBoot0/src/main/webapp/WEB-INF/result.jsp
@@ -7,18 +7,7 @@
 <body>
 <h1>Hello, JSP World!</h1>
 
-<%
-    Integer sum = (Integer) request.getSession().getAttribute("sum");
-    if (sum != null) {
-%>
-<p>The sum is: <%= sum %></p>
-<%
-} else {
-%>
-<p>No result available.</p>
-<%
-    }
-%>
+    Result is : ${sum}
 
 </body>
 </html>
