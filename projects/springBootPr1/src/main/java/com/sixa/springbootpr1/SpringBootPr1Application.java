e548720ab46cbf9a1f0fc8ad1b6b4f7030d4a480
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-10-24 14:24:33 +0200

    lets start

---
+++ projects/springBootPr1/src/main/java/com/sixa/springbootpr1/SpringBootPr1Application.java
@@ -0,0 +1,13 @@
+package com.sixa.springbootpr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPr1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPr1Application.class, args);
+    }
+
+}
---
+++ projects/springBootPr1/src/test/java/com/sixa/springbootpr1/SpringBootPr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixa.springbootpr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPr1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
