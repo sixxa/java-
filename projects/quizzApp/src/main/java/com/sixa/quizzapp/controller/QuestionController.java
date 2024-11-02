5101a3da10524d93ddb65c3db1012be4f362376c
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-11-02 19:27:45 +0100

    developing question service

---
+++ projects/quizzApp/src/main/java/com/sixa/quizzapp/controller/QuestionController.java
@@ -0,0 +1,31 @@
+package com.sixa.quizzapp.controller;
+
+import com.sixa.quizzapp.model.Question;
+import com.sixa.quizzapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+}
---
+++ projects/quizzApp/src/main/java/com/sixa/quizzapp/service/QuestionService.java
@@ -0,0 +1,28 @@
+package com.sixa.quizzapp.service;
+
+import com.sixa.quizzapp.dao.QuestionDao;
+import com.sixa.quizzapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
+    }
+}
