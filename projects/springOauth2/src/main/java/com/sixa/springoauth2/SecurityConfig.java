6b4115bba0f877eaa5b6b4ede8be31a8c547d44a
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-11-02 14:30:09 +0100

    Oauth2 setup done

---
+++ projects/springOauth2/src/main/java/com/sixa/springoauth2/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.sixa.springoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
