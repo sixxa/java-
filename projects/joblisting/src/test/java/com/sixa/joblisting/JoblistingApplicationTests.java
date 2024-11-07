92fd9a75effe22713f1451be83fe624ffcf36bc1
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-11-07 13:51:25 +0100

    started developing mongoDB project

---
+++ projects/joblisting/src/test/java/com/sixa/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixa.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
