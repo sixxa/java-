f32acc83dccdbe63069917389a62de5f5e918eee
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-11-16 18:44:19 +0100

    jwt

---
+++ spring/security/JWTAuth/src/main/java/com/sixa/jwtauth/config/JwtService.java
@@ -0,0 +1,74 @@
+package com.sixa.jwtauth.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "mtCKGX/Mzg1qcr1iKrdvDmtPrEiRxqURaUD6eJ/ukRc1Agoc46s6YWMNzJP0oOdsy+2cNTa1hDA//70FYnxHGqVS3pbenaCKI4JdFKrUa+FoSNQFaIJbzpKLDhK+lnAVuB7ypgJ2eF1oVdHAdE/FrjTdRciEgLMYt9LlbSI8Zj77gAKvhC2NKyem3YXgP6rUXzJJKsjXsPwJyPqjfZy2dGThi/ziXt6RaIO8N0uJf+CkTCUBPXzgOnLSh/1rQqUt7XgsFsb8PtiIVCbjRUtbpLiQzFQj1Cd96dedJY723t3odZ3o4Mf9yd7YZTL779QICuPEah9DjhZyuBBR2C9et9QpQnjGfLSOH9pcWboDbb0=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSighInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSighInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getBody();
+    }
+
+    private Key getSighInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
