69905214ac38da5cbb6ae6917db66596c7a3ea4b
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2024-11-17 12:08:19 +0100

    finished jwt adding registration

--- spring/security/JWTAuth/src/main/java/com/sixa/jwtauth/config/JwtAuthenticationFilter.java
+++ spring/security/JWTAuth/src/main/java/com/sixa/jwtauth/config/JwtAuthenticationFilter.java
@@ -51,5 +51,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        filterChain.doFilter(request, response);
     }
 }
