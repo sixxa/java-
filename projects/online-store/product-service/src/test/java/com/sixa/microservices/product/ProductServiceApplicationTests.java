da49af37ff4637b205895876a938b41d601fb040
Tornike Sikharulidze <tokkasikharulidze@gmail.com>
2025-01-03 22:39:38 +0100

    product-service miscroservice done

--- projects/online-store/product-service/src/test/java/com/sixa/microservices/product/ProductServiceApplicationTests.java
+++ projects/online-store/product-service/src/test/java/com/sixa/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.sixa.microservices.product;
 
+import com.mongodb.client.MongoClient;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.math.MathContext;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void shouldCreateProduct() {
+        String requestBody = """
+                    {
+                      "name" : "Iphone 14",
+                      "description" : "ram/storage 8/256",
+                      "price" : 1500
+                    }
+               """;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Iphone 14"))
+                .body("description", Matchers.equalTo("ram/storage 8/256"))
+                .body("price", Matchers.equalTo(1500));
     }
 
 }
+
+
